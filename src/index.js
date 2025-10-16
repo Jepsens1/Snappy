9f5455d0599d7ba58df1fd7891ea82c2333c183f
Phillip Jepsen <jepsens123@outlook.dk>
2025-10-16 14:16:39 +0200

    fix indentation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const token = process.env.BOT_TOKEN;
 
 // Create a new client instance
-const client = new Client({intents: [GatewayIntentBits.Guilds]});
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
 
@@ -14,35 +13,35 @@ const commandFolders = fs.readdirSync(foldersPath);
 
 // Loops through each subdirectory found inside commands
 for (const folder of commandFolders) {
-    // Reads the directory path
-    const commandsPath = path.join(foldersPath, folder);
-    // Gets all files ending with .js
-    const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
-    for (const file of commandFiles) {
-        // Reads the .js file path
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
+	// Reads the directory path
+	const commandsPath = path.join(foldersPath, folder);
+	// Gets all files ending with .js
+	const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		// Reads the .js file path
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
 
-        if ('data' in command && 'execute' in command) {
-            client.commands.set(command.data.name, command)
-        } else {
-            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-        }
-    }
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
 }
 
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith('.js'));
 
 for (const file of eventFiles) {
-    const filePath = path.join(eventsPath, file);
-    const event = require(filePath);
-    if (event.once) {
-        client.once(event.name, (...args) => event.execute(...args));
-    } else {
-        client.on(event.name, (...args) => event.execute(...args));
-    }
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
 }
 
 // Log in to Discord with bot token
-client.login(token);
+client.login(process.env.BOT_TOKEN);
