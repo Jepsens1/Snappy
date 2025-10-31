9f4ab5b3a06c7bc6f5fa7ffe27cb5e2f927a8b35
Phillip Jepsen <jepsens123@outlook.dk>
2025-10-31 16:09:51 +0100

    Add Roles command to display all roles in guild

---
+++ src/commands/utility/roles.js
@@ -0,0 +1,34 @@
+const {
+	SlashCommandBuilder,
+	InteractionContextType,
+	EmbedBuilder,
+} = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('roles')
+		.setDescription('display roles and permission for this guild')
+		.setContexts(InteractionContextType.Guild),
+	/**
+	 * @param {import('discord.js').Interaction} interaction
+	 */
+	async execute(interaction) {
+		await interaction.deferReply();
+
+		try {
+			const roles = await interaction.guild.roles.fetch();
+			const roleNames = roles.map((role) => role.toString());
+			const roleEmbed = new EmbedBuilder()
+				.setTitle(`Roles [${roles.size}]`)
+				.setColor('Blue')
+				.addFields({
+					name: '',
+					value: `${roleNames}`,
+				});
+			await interaction.editReply({ embeds: [roleEmbed] });
+		} catch (error) {
+			await interaction.editReply('Unknown error occured during /roles');
+			console.error('Error happened', error);
+		}
+	},
+};
